90cccecd32e328858c74fd0f7d6e6dd0e6bc9c58
Alexander Seton <alexanderseton@protonmail.com>
2021-12-19 23:44:06 +0000

    Added new route to Heroku database try 3.

--- src/ScoresService.js
+++ src/ScoresService.js
@@ -1,20 +1,16 @@
 const baseURL = "https://mines-game-server.herokuapp.com/api/scores"
 
-export const getScores = () => {
-    return(
-        fetch(baseURL)
-        .then(result => result.json())
-        .then(data => result.results)
-    )
+export const getScores = async () => {
+    const response = await fetch(baseURL);
+    const data = await response.results;
+    console.log("data:", data);
+    return data;
 }
 
-export const postScore = (high_score) => {
-    return(
-        fetch(baseURL, {
+export const postScore = async (high_score) => { 
+        await fetch(baseURL, {
             method: "POST",
             body: JSON.stringify(high_score),
             headers: {"Content-Type": "application/json"}
         })
-        .then(result => result.json())
-    )
 }
