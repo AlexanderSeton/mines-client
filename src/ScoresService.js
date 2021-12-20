2e1aab5d320c8ad0123a0874795b72b25fb645ee
Alexander Seton <alexanderseton@protonmail.com>
2021-12-20 00:01:17 +0000

    Fixing logic error get request.

--- src/ScoresService.js
+++ src/ScoresService.js
@@ -2,9 +2,11 @@ const baseURL = "https://mines-game-server.herokuapp.com/api/scores"
 
 export const getScores = async () => {
     const response = await fetch(baseURL);
-    const data = await response.results;
+    const data = await response.json();
     console.log("data:", data);
-    return data;
+    const scores = await data.results;
+    console.log("scores:", scores);
+    return scores;
 }
 
 export const postScore = async (high_score) => { 
